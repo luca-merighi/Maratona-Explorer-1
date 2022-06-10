f8db5d383acab3ce9bd4b7aaebb15ddfbe13dc3f
Luca Merighi <73050019+luca-merighi@users.noreply.github.com>
2022-06-10 10:48:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+const answer = document.querySelector('#answer')
+const questionInput = document.querySelector('#question-input')
+
+const answers = [
+    "Certeza!",
+    "Não tenho tanta certeza.",
+    "É decididamente assim.",
+    "Não conte com isso.",
+    "Sem dúvidas!",
+    "Pergunte novamente mais tarde.",
+    "Sim, definitivamente!",
+    "Minha resposta é não.",
+    "Você pode contar com isso.",
+    "Melhor não te dizer agora.",
+    "A meu ver, sim.",
+    "Minhas fontes dizem não.",
+    "Provavelmente.",
+    "Não é possível prever agora.",
+    "Perspectiva boa.",
+    "As perspectivas não são tão boas.",
+    "Sim.",
+    "Concentre-se e pergunte novamente.",
+    "Sinais apontam que sim."
+]
+
+function question() {
+    if(questionInput.value == '') {
+        alert('Digite sua pergunta')
+        return
+    }
+
+    const question = "<div>" + questionInput.value + "</div>"
+    
+    const totalAnswers = answers.length
+    const randomNumber = Math.floor(Math.random() * totalAnswers)
+    
+    answer.innerHTML = question + answers[randomNumber]    
+    
+    setTimeout(function() {
+        questionInput.value = ''
+        answer.style.opacity = 0       
+    }, 3000)
+}
